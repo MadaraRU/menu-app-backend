592c9af4c889ad9b5fe3cbdcfc435ad273d4ae8a
MadaraRU <78321649+MadaraRU@users.noreply.github.com>
2022-07-13 08:43:04 +0100

    backend

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,5 @@
 import { RequestHandler } from "express";
 import asyncHandler from "express-async-handler";
-// import bcrypt from "bcryptjs";
 import generateToken from "../utils/generateToken";
 import User from "../models/userModel";
 
@@ -23,17 +22,11 @@ export const registerUser: RequestHandler = asyncHandler(async (req, res) => {
     throw new Error("User already exists");
   }
 
-  // Hash password
-  // const salt = await bcrypt.genSalt(10);
-  // const hashedPassword = await bcrypt.hash(password, salt);
-
   // Create user
   const user = await User.create({
     name,
     email,
     password,
-
-    // password: hashedPassword,
   });
 
   if (user) {
@@ -58,12 +51,6 @@ export const authUser: RequestHandler = asyncHandler(async (req, res) => {
 
   const user = await User.findOne({ email });
 
-  // if (user && (await bcrypt.compare(password, user.password))) {
-  //   res.json({
-  //     _id: user._id,
-  //     email: user.email,
-  //     token: generateToken(user._id),
-  //   });
   if (user) {
     res.json({
       _id: user._id,
